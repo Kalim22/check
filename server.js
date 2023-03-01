55feb97ea0424520dad2deeca410e659fd2d8f2f
kalim22 <kalimali2295@gmail.com>
2023-03-01 12:54:03 +0530

    get and post apI

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/use", (req, res) => {
+  return res.send("use api");
+});
+
+app.get("/data-save/:name", (req, res) => {
+  let a = req.params.name;
+  return res.send(`hello ${a}`);
+});
+
+app.post("/post", (req, res) => {
+  let name = req.body.name;
+  let age = req.body.age;
+
+  return res
+    .status(200)
+    .json({ msg: `hello world ${req.body.name} and ${req.body.age}` });
+});
+
 app.listen("9000", (err) => {
   if (err) {
     console.log(err);
